a2a5103f3841fc3bb5a17257e220dec2c7b3ccb4
atsuc1973 <atsu1973@gmail.com>
2021-08-29 20:54:10 +0900

    implement cleanup1

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -144,81 +144,79 @@ export default function Header(props) {
   };
 
   const menuOptions = [
-    {name: "Services", link: "/services"},
-    {name: "Custom Software Development", link: "/customsoftware"},
-    {name: "Mobile App Development", link: "/mobileapps"},
-    {name: "Website Development", link: "/websites"}
+    {
+      name: "Services", 
+      link: "/services", 
+      activeIndex: 1, 
+      selectedIndex: 0
+    },
+    {
+      name: "Custom Software Development", 
+      link: "/customsoftware", 
+      activeIndex: 1, 
+      selectedIndex: 1
+    },
+    {
+      name: "Mobile App Development", 
+      link: "/mobileapps", 
+      activeIndex: 1, 
+      selectedIndex: 2
+    },
+    {
+      name: "Website Development", 
+      link: "/websites", 
+      activeIndex: 1, 
+      selectedIndex: 3
+    }
+  ]
+
+  const routes = [
+    {
+      name: "Home", 
+      link: "/",
+      activeIndex: 0
+    },
+    {
+      name: "Services", 
+      link: "/services",
+      activeIndex: 1,
+      ariaOwns: anchorEl ? "simple-menu" : undefined,
+      ariaPopup: anchorEl ? "true" : undefined,
+      mouseOver: event => handleClick(event)
+    },
+    {
+      name: "The Revolution", 
+      link: "/revolution",
+      activeIndex: 2
+    },
+    {
+      name: "About Us", 
+      link: "/about",
+      activeIndex: 3
+    },
+    {
+      name: "Contact Us", 
+      link: "/contact",
+      activeIndex: 4
+    }
   ]
 
   useEffect(() => {
-    if (window.location.pathname === "/" && value !== 0) {
-      setValue(0)
-    } else if (window.location.pathname === "/services" && value !== 1) {
-      setValue(1)
-    } else if (window.location.pathname === "/revolution" && value !== 2) {
-      setValue(2)
-    } else if (window.location.pathname === "/about" && value !== 3) {
-      setValue(3)
-    } else if (window.location.pathname === "/contact" && value !== 4) {
-      setValue(4)
-    } else if (window.location.pathname === "/estimate" && value !== 5) {
-      setValue(5)
-    }
-
-    switch (window.location.pathname) {
-      case "/":
-        if (value !== 0) {
-          setValue(0)
-        }
-        break;
-      case "/services":
-        if (value !== 1) {
-          setValue(1);
-          setSelectedIndex(0);
-        }
-        break;
-      case "/customsoftware":
-        if (value !== 1) {
-          setValue(1);
-          setSelectedIndex(1);
-        }
-        break;
-      case "/mobileapps":
-        if (value !== 1) {
-          setValue(1);
-          setSelectedIndex(2);
-        }
-        break;
-      case "/websites":
-        if (value !== 1) {
-          setValue(1);
-          setSelectedIndex(3);
-        }
-        break;
-      case "/revolution":
-        if (value !== 2) {
-          setValue(2);
-        }
-        break;
-      case "/about":
-        if (value !== 3) {
-          setValue(3);
-        }
-        break;
-      case "/contact":
-        if (value !== 4) {
-          setValue(4);
-        }
-        break;
-      case "/estimate":
-        if (value !== 5) {
-          setValue(5);
-        }
-        break;
-      default:
-        break;
-    }
-  }, [value]);
+    [...menuOptions, ...routes].forEach(route => {
+      switch (window.location.pathname) {
+        case `${route.link}`:
+          if (value !== route.activeIndex) {
+            setValue(route.activeIndex)
+            if (route.selectedIndex && route.selectedIndex !== selectedIndex) {
+              setSelectedIndex(route.selectedIndex)
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    })
+  }, [value, menuOptions, selectedIndex, routes]);
 
   const tabs = (
     <React.Fragment>
@@ -228,38 +226,15 @@ export default function Header(props) {
         className={classes.tabContainer}
         indicatorColor="primary"
       >
+        {routes.map((route, index) => (
+          <Tab className={classes.tab} component={Link} to={route.link} 
+          label={route.name} aria-owns={route.ariaOwns} aria-haspopup={route.ariaPopup} onMouseOver={route.mouseOver} />
+        ))}
         <Tab 
           className={classes.tab} 
           component={Link} 
           to="/" 
           label="Home" 
-        />
-        <Tab 
-          aria-owns={anchorEl ? "simple-menu" : undefined}
-          aria-haspopup={anchorEl ? "true" : undefined}
-          className={classes.tab} 
-          component={Link} 
-          onMouseOver={(event) => handleClick(event)}
-          to="/services" 
-          label="Services" 
-        />
-        <Tab 
-          className={classes.tab} 
-          component={Link} 
-          to="/revolution" 
-          label="The Revolution" 
-        />
-        <Tab 
-          className={classes.tab} 
-          component={Link}  
-          to="/about" 
-          label="About Us" 
-        />
-        <Tab 
-          className={classes.tab} 
-          component={Link} 
-          to="/contact" 
-          label="Contact Us" 
         />
       </Tabs>
       <Button 
