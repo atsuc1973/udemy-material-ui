929816f902e537633f7c801cc37e3c6319019bfb
atsuc1973 <atsu1973@gmail.com>
2021-09-02 04:11:20 +0900

    implement using Grid

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -123,13 +123,11 @@ export default function Header(props) {
   const matches = useMediaQuery(theme.breakpoints.down("md"));
 
   const [openDrawer, setOpenDrawer] = useState(false);
-  const [value, setValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
   const [openMenu, setOpenMenu] = useState(false);
-  const [selectedIndex, setSelectedIndex] = useState(0)
 
   const handleChange = (e, newValue) => {
-    setValue(newValue);
+    props.setValue(newValue);
   };
 
   const handleClick = (e) => {
@@ -140,7 +138,7 @@ export default function Header(props) {
   const handleMenuItemClick = (e, i) => {
     setAnchorEl(null);
     setOpenMenu(false);
-    setSelectedIndex(i);
+    props.setSelectedIndex(i);
   };
 
   const handleClose = (e) => {
@@ -205,10 +203,10 @@ export default function Header(props) {
     [...menuOptions, ...routes].forEach(route => {
       switch (window.location.pathname) {
         case `${route.link}`:
-          if (value !== route.activeIndex) {
-            setValue(route.activeIndex)
-            if (route.selectedIndex && route.selectedIndex !== selectedIndex) {
-              setSelectedIndex(route.selectedIndex)
+          if (props.value !== route.activeIndex) {
+            props.setValue(route.activeIndex)
+            if (route.selectedIndex && route.selectedIndex !== props.selectedIndex) {
+              props.setSelectedIndex(route.selectedIndex)
             }
           }
           break;
@@ -216,12 +214,12 @@ export default function Header(props) {
           break;
       }
     })
-  }, [value, menuOptions, selectedIndex, routes]);
+  }, [props.value, menuOptions, props.selectedIndex, routes, props]);
 
   const tabs = (
     <React.Fragment>
       <Tabs 
-        value={value} 
+        value={props.value} 
         onChange={handleChange}
         className={classes.tabContainer}
         indicatorColor="primary"
@@ -268,10 +266,10 @@ export default function Header(props) {
             classes={{root: classes.menuItem}}
             onClick={(event)=>{
               handleMenuItemClick(event, i); 
-              setValue(1); 
+              props.setValue(1); 
               handleClose()
             }}
-            selected={i === selectedIndex && value === 1}
+            selected={i === props.selectedIndex && props.value === 1}
           >
             {option.name}
           </MenuItem>
@@ -299,11 +297,11 @@ export default function Header(props) {
               button 
               component={Link} 
               to={route.link} 
-              selected={ value === route.activeIndex } 
+              selected={ props.value === route.activeIndex } 
               cllases={{selected: classes.drawerItemSelected}}
               onClick={() => {
                 setOpenDrawer(false); 
-                setValue(route.activeIndex)
+                props.setValue(route.activeIndex)
               }}
             >
               <ListItemText 
@@ -315,11 +313,14 @@ export default function Header(props) {
             </ListItem>
           ))}
           <ListItem 
-            onClick={() => {setOpenDrawer(false); setValue(5)}} 
+            onClick={() => {
+              setOpenDrawer(false); 
+              props.setValue(5)
+            }} 
             divider button component={Link} 
             classes={{root: classes.drawerItemEstimate, selected: classes.drawerItemSelected}}
             to="/estimate"
-            selected={value === 5}
+            selected={props.value === 5}
           >
             <ListItemText 
               className={classes.drawerItem} 
@@ -349,7 +350,7 @@ export default function Header(props) {
               component={Link} 
               to="/" 
               disableRipple
-              onClick={() => setValue(0)}
+              onClick={() => props.setValue(0)}
               className={classes.logoContainer}
             >
               <img alt="company logo" className={classes.logo}  src={logo} />
